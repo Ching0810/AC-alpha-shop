72b99de15421db406feb38503e5a8a02054e8074
Ching0810 <neinchin123@gmail.com>
2023-09-28 17:40:53 +0800

    built: project init & static page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { ProgressControl } from './component/ProgressControl/ProgressControl.jsx'
+import { StepOne } from './component/StepOne/StepOne.jsx'
+import { StepProgress } from './component/StepProgress/StepProgress.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <ProgressControl />
+      <StepOne />
+      <StepProgress />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/ProgressControl/ProgressControl.jsx
@@ -0,0 +1,33 @@
+// import complete from '../../assets/icons/pg-complete.svg'
+import style from './ProgressControl.module.css'
+
+export function ProgressControl () {
+  return (
+    <div className={style.container}>
+      <div className={style.title}>結帳</div>
+      <div className={style.progressContainer}>
+        <div className={style.progressOne}>
+          <div className={style.progressIcon}>
+            <span>1</span>
+            {/* <img className={style.img} src={complete} alt="" /> */}
+          </div>
+          <span>寄送地址</span>
+        </div>
+        <div className={style.progressBar}></div>
+        <div className={style.progressTwo}>
+          <div className={style.progressIconDefault}>
+            <span>2</span>
+          </div>
+          <span className={style.default}>運送方式</span>
+        </div>
+        <div className={style.progressBar}></div>
+        <div className={style.progressThree}>
+          <div className={style.progressIconDefault}>
+            <span>3</span>
+          </div>
+          <span className={style.default}>付款資訊</span>
+        </div>
+      </div>
+    </div>
+  )
+}
