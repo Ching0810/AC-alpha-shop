7dbbe146ee709d34ed199b8c49bf25a8180ca81e
Ching0810 <neinchin123@gmail.com>
2023-10-29 00:57:06 +0800

    build: finished Cart component total cost function

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,13 +3,22 @@ import { data } from './data.js'
 import CartItem from './CartItem.jsx'
 import plus from '../../assets/icons/plus.svg'
 import minus from '../../assets/icons/minus.svg'
+import { useState } from 'react'
 
 export default function Cart () {
+  const [totalCost, setTotalCost] = useState({})
+  function calculateTotalCost(count, data) {
+    const productCost = (count * data.price)
+    return productCost
+  }
+
+  const totalCostSum = Object.values(totalCost).reduce((a, b) => a + b, 0)
+
   return (
     <div className={ style.container }>
       <h3 className={ style.title }>購物籃</h3>
       {data.map(data => 
-        <CartItem key={ data.id } data={ data } plus={ plus } minus={ minus }/>
+        <CartItem key={ data.id } data={ data } plusImg={ plus } minusImg={ minus } setTotalCost={ setTotalCost } calculateTotalCost={calculateTotalCost}/>
       )}
       <div className={ `${style.cartInfo} ${style.shipping}` }>
         <div>運費</div>
@@ -17,7 +26,7 @@ export default function Cart () {
       </div>
       <div className={ style.cartInfo }>
         <div>小計</div>
-        <div>$0</div>
+        <div>${totalCostSum}</div>
       </div>
     </div>
   )
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,13 +1,31 @@
 import PropTypes from 'prop-types';
 import style from './Cart.module.css'
+import { useState } from 'react';
 
 CartItem.propTypes = {
   data: PropTypes.object.isRequired,
-  plus: PropTypes.string.isRequired,
-  minus: PropTypes.string.isRequired,
+  plusImg: PropTypes.string.isRequired,
+  minusImg: PropTypes.string.isRequired,
+  setTotalCost: PropTypes.func.isRequired,
+  calculateTotalCost: PropTypes.func.isRequired,
 };
 
-export default function CartItem ( { data, plus, minus } ) {
+export default function CartItem ( { data, plusImg, minusImg, setTotalCost, calculateTotalCost } ) {
+  const [count, setCount] = useState(0)
+  function handleMinusClick() {
+    if (count > 0) {
+      setCount(count - 1)
+      const newTotalCost = calculateTotalCost(count - 1, data)
+      setTotalCost(prevTotalCost => ({ ...prevTotalCost, [data.id]: newTotalCost }))
+    }
+  }
+
+  function handlePlusClick() {
+    setCount(count + 1)
+    const newTotalCost = calculateTotalCost(count + 1, data)
+    setTotalCost(prevTotalCost => ({ ...prevTotalCost, [data.id]: newTotalCost }))
+  }
+
   return (
     <div className={ style.cartItem } key={ data.id }>
       <img className={ style.productImg } src={ data.img }/>
@@ -15,9 +33,9 @@ export default function CartItem ( { data, plus, minus } ) {
         <h4 className={ style.name }>{ data.name }</h4>
         <p className={ style.price }>${ data.price }</p>
           <div className={ style.quantityControl }>
-          <img src={minus} className={ style.minus } />
-          <div className={ style.quantity }>{ data.quantity }</div>
-          <img src={plus} className={ style.plus } />
+          <img src={minusImg} className={ style.minus } onClick={handleMinusClick}/>
+          <div className={ style.quantity }>{ count }</div>
+          <img src={plusImg} className={ style.plus } onClick={handlePlusClick}/>
         </div>
       </div>
     </div>
