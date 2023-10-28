c575e6b1ddf004852c8a646439291b66e4232d69
Ching0810 <neinchin123@gmail.com>
2023-10-28 19:02:51 +0800

    build: finished StepThree component

--- src/components/StepThree/StepThree.jsx
+++ src/components/StepThree/StepThree.jsx
@@ -1,7 +1,33 @@
+import style from './StepThree.module.css'
+
 export default function StepThree() {
   return (
-    <div>
-      
+    <div className={style.container}>
+      <div className={style.title}>付款資訊</div>
+      <div className={style.formContainer}>
+        <div className={style.formRowContainer}>
+          <label className={style.inputContainer}>
+            <div>持卡人姓名</div>
+            <input className={style.input} type='text' placeholder='John Doe'></input>
+          </label>
+        </div>
+        <div className={style.formRowContainer}>
+          <label className={style.inputContainer}>
+            <div>卡號</div>
+            <input className={style.input} type='text' placeholder='1111 2222 3333 4444' maxLength={16}></input>
+          </label>
+        </div>
+        <div className={`${style.formRowContainer} ${style.twoInput}`}>
+            <label className={style.inputContainer}>
+              <div>有效日期</div>
+              <input className={style.input} type='date'></input>
+            </label>
+            <label className={style.inputContainer}>
+              <div>CVC / CCV</div>
+              <input className={style.input} type='text' placeholder='123' maxLength={3}></input>
+            </label>
+        </div>
+      </div>
     </div>
   )
 }
